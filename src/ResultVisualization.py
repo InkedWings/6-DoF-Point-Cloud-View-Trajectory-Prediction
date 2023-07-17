42bd73662c700bcf10a6c0a91027227a4d7fe576
Zhijing Ye <96925620+InkedWings@users.noreply.github.com>
2023-07-17 14:37:25 -0400

    Update ResultVisualization.py

--- src/ResultVisualization.py
+++ src/ResultVisualization.py
@@ -26,36 +26,100 @@ class ResultVisualization:
         plt.legend()
         plt.show()
 
-    def data_plot(self, groundtruth, prediction):
+    """
+    zoomInRange is 0 in default, the plot will show the whole range
+    If zoomInRange is not 0, plot will show the given range
+    zoomInBias is 0 in default, the plot will begin from index 0
+    If zoomInBias is given and not equal to 0, the plot will start from the index %zoomInBias
+    We limit the range of y-aixs of the graph according to the min and max of the whole sequence.
+    ymin and ymax should be an array in size of 3
+    """
+    def data_plot(self, groundtruth, prediction, zoomInRange, zoomInBias):
         plt.figure()
-        groundtruth_index = np.arange(0, self.observ_step + self.pred_step)
-        # enc_output_index = np.arange(1, self.observ_step + 1)
-        pred_index = np.arange(self.observ_step, self.observ_step + self.pred_step)
+        if zoomInRange != 0:
+            groundtruth_index = np.arange(zoomInBias, self.observ_step + zoomInBias + zoomInRange)
+            pred_index = np.arange(self.observ_step + zoomInBias, self.pred_step + zoomInBias + zoomInRange)
+        else:
+            groundtruth_index = np.arange(0, self.observ_step + self.pred_step)
+            pred_index = np.arange(self.observ_step, self.observ_step + self.pred_step)
         if self.mode == "angle":
             subtitles = ["pitch", "yaw", "roll"]
         elif self.mode == "position":
             subtitles = ["x-axis", "y-axis", "z-axis"]
+        """
+        Result Denormalization
+        """
+        # groundtruth =groundtruth * std + mean
+        # prediction = prediction * std + mean
 
         Ylabel = 'degree' if self.mode == "Angular" else 'unit'
         plt.title("%s prediction for %s in period of %ds" % (self.architecture, self.mode, self.pred_step))
         plt.subplot(3, 1, 1)
+        # plt.ylim(ymin[0], ymax[0])
         plt.title(subtitles[0])
         plt.plot(groundtruth_index, groundtruth[:, 0], 'b', label='GroundTruth')
-        # plt.plot(enc_output_index, enc_output[:, 0], 'g', label = 'EncoderOutput')
         plt.plot(pred_index, prediction[:, 0], 'r', label='Prediction')
         plt.xlabel('sample index')
         plt.ylabel(Ylabel)
         plt.subplot(3, 1, 2)
+        # plt.ylim(ymin[1], ymax[1])
         plt.title(subtitles[1])
         plt.plot(groundtruth_index, groundtruth[:, 1], 'b', label='GroundTruth')
-        # plt.plot(enc_output_index, enc_output[:, 1], 'g', label='EncoderOutput')
         plt.plot(pred_index, prediction[:, 1], 'r', label='Prediction')
         plt.xlabel('sample index')
         plt.ylabel(Ylabel)
         plt.subplot(3, 1, 3)
+        # plt.ylim(ymin[2], ymax[2])
         plt.title(subtitles[2])
         plt.plot(groundtruth_index, groundtruth[:, 2], 'b', label='GroundTruth')
-        # plt.plot(enc_output_index, enc_output[:, 2], 'g', label='EncoderOutput')
+        plt.plot(pred_index, prediction[:, 2], 'r', label='Prediction')
+        plt.xlabel('sample index')
+        plt.ylabel(Ylabel)
+        plt.legend()
+        plt.show()
+
+    def data_plot2(self, groundtruth, enc_output, prediction, zoomInRange, zoomInBias):
+        plt.figure()
+        if zoomInRange != 0:
+            groundtruth_index = np.arange(zoomInBias, self.observ_step + zoomInBias + zoomInRange)
+            pred_index = np.arange(self.observ_step + zoomInBias, self.observ_step + zoomInBias + zoomInRange)
+        else:
+            groundtruth_index = np.arange(0, self.observ_step + self.pred_step)
+            enc_output_index = np.arange(1, self.observ_step + 1)
+            pred_index = np.arange(self.observ_step, self.observ_step + self.pred_step)
+        if self.mode == "angle":
+            subtitles = ["pitch", "yaw", "roll"]
+        elif self.mode == "position":
+            subtitles = ["x-axis", "y-axis", "z-axis"]
+        """
+        Result Denormalization
+        """
+        # groundtruth =groundtruth * std + mean
+        # prediction = prediction * std + mean
+
+        Ylabel = 'degree' if self.mode == "Angular" else 'unit'
+        plt.title("%s prediction for %s in period of %ds" % (self.architecture, self.mode, self.pred_step))
+        plt.subplot(3, 1, 1)
+        # plt.ylim(ymin[0], ymax[0])
+        plt.title(subtitles[0])
+        plt.plot(groundtruth_index, groundtruth[:, 0], 'b', label='GroundTruth')
+        plt.plot(enc_output_index, enc_output[:, 0], 'g', label = 'EncoderOutput')
+        plt.plot(pred_index, prediction[:, 0], 'r', label='Prediction')
+        plt.xlabel('sample index')
+        plt.ylabel(Ylabel)
+        plt.subplot(3, 1, 2)
+        # plt.ylim(ymin[1], ymax[1])
+        plt.title(subtitles[1])
+        plt.plot(groundtruth_index, groundtruth[:, 1], 'b', label='GroundTruth')
+        plt.plot(enc_output_index, enc_output[:, 1], 'g', label='EncoderOutput')
+        plt.plot(pred_index, prediction[:, 1], 'r', label='Prediction')
+        plt.xlabel('sample index')
+        plt.ylabel(Ylabel)
+        plt.subplot(3, 1, 3)
+        # plt.ylim(ymin[2], ymax[2])
+        plt.title(subtitles[2])
+        plt.plot(groundtruth_index, groundtruth[:, 2], 'b', label='GroundTruth')
+        plt.plot(enc_output_index, enc_output[:, 2], 'g', label='EncoderOutput')
         plt.plot(pred_index, prediction[:, 2], 'r', label='Prediction')
         plt.xlabel('sample index')
         plt.ylabel(Ylabel)
